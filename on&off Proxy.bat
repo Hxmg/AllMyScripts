d9eeb8c5aac787a2cd18eb1fdfa1269ec45f28fe
大萝北子 <37783752+Hxmg@users.noreply.github.com>
2020-10-16 19:10:20 +0800

    Add files via upload
    One-Key Enable&Disabled Windows System Proxy

---
+++ on&off Proxy.bat
@@ -0,0 +1,20 @@
+@echo off
+
+For /f "tokens=3" %%i in ('Reg Query "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyEnable" ') do Set a=%%i
+
+if %a% == 0x1 (
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f 
+echo 系统代理已经关闭
+goto endd
+) else (
+
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f 
+echo 系统代理已经打开
+
+)
+
+For /f  "tokens=3" %%i in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer') do Set a=%%i
+echo %a%
+
+:endd
+pause
